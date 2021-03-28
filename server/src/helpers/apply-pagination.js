0cbfc2c1ded98e5b76cf07419bdf3eb147a63134
Benjamin Domey <domeybenjamin1@gmail.com>
2021-03-28 17:53:48 +0000

    removed

--- server/src/helpers/apply-pagination.js
+++
@@ -1,8 +0,0 @@
-const applyPagination = model => condition => (skip, limit) =>
-  model
-    .find(condition)
-    .skip(skip)
-    .limit(limit)
-    .sort({ updatedAt: -1 });
-
-module.exports = applyPagination;
