5eae058b999fd5a4be49851d8a32ba986caab206
Benjamin Domey <domeybenjamin1@gmail.com>
2021-04-06 08:32:27 +0000

    removed entity

--- server/src/domains/entity/index.js
+++
@@ -1,8 +0,0 @@
-const { EntityModel } = require('./model');
-
-module.exports = {
-  createEntity: args => new EntityModel(args).save(),
-  getEntities: ({ filter = {} }) => EntityModel.find(filter),
-  getEntitiesLength: ({ filter = {} }) =>
-    EntityModel.countDocuments(filter)
-};
