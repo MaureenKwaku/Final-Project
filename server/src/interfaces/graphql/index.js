dc10e120b707dba203a3c8bd9f4dc15a623c2b26
Benjamin Domey <domeybenjamin1@gmail.com>
2021-04-07 17:43:20 +0000

    made sure I added graphql for resolver and query

--- server/src/interfaces/graphql/index.js
+++ server/src/interfaces/graphql/index.js
@@ -1,16 +1,17 @@
-const { admin, user, car } = require('../../domains');
+const { admin, user, car, rental, payment } = require('../../domains');
 
 exports.resolvers = {
   Mutation: {
     ...admin.resolver.Mutation,
     ...user.resolver.Mutation,
     ...car.resolver.Mutation,
+    ...rental.resolver.Mutation,
   },
   Query: {
     ...admin.resolver.Query,
     ...user.resolver.Query,
-    ...car.resolver.Query,
+    ...rental.resolver.Query,
   },
 };
 
-exports.typeDefs = [admin.schema, user.schema, car.schema];
+exports.typeDefs = [admin.schema, user.schema, car.schema, rental.schema, payment.schema];
