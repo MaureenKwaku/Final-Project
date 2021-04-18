d99b33cec9855f167b5841ce4707af448de6de5a
Benjamin Domey <domeybenjamin1@gmail.com>
2021-04-18 22:58:45 +0000

    added make changes

--- client/pages/car/[id].js
+++ client/pages/car/[id].js
@@ -91,11 +91,30 @@ const Car = () => {
                     / {data?.car?.make}
                   </p>
                   <h3 className="font-bold text-3xl text-gray-700 my-5">
-                    {data?.car?.make} {data?.car?.model}
+                    {data?.car?.model}
                   </h3>
                   <h3 className={"font-medium"}>
                     GH&cent; {(data?.car?.price / 100).toFixed(2)} per hour
                   </h3>
+
+                  <h3 className={"flex flex-row mt-5"}>
+                    Make{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5 text-gray-600 ml-1"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </h3>
+                  <p className={"font-light text-sm -ml-1"}>
+                    {data?.car?.make}
+                  </p>
 
                   <h3 className={"flex flex-row mt-5"}>
                     Plate Number{" "}
