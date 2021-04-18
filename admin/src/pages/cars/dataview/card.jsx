56bfafeae9fcce9227d15d458914013eff177b90
Benjamin Domey <domeybenjamin1@gmail.com>
2021-04-18 21:40:21 +0000

    added the edit icon

--- admin/src/pages/cars/dataview/card.jsx
+++ admin/src/pages/cars/dataview/card.jsx
@@ -75,8 +75,8 @@ const AdminCard = ({ data, view, edit }) => {
                 className="relative w-0 border-l flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium  rounded-br-lg hover:text-gray-500"
               >
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
                   className="w-5 h-5 text-gray-400"
+                  xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -85,13 +85,7 @@ const AdminCard = ({ data, view, edit }) => {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     strokeWidth={2}
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                    d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
                   />
                 </svg>
 
